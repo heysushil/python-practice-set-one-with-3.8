231a456a297b554be6d35632446d6ce814d6f9c9
heysushil <sushil.chaudhary111@gmail.com>
2020-09-09 14:26:16 +0530

    function work

--- 17.function.py
+++ 17.function.py
@@ -20,6 +20,8 @@ Pre-defined function:
 
 User-defined function:
 
+Note: funciton create karne ke liye 'def' keyword ka use karte hain.
+
 1. User-define funciton ka name user define karta hai.
 2. Funciton name hamesa relevent name hona cahiye. Taki name se function ka work behaviour pata chale.
 3. Function ke sub types bhi hain:
@@ -29,4 +31,30 @@ User-defined function:
     d. Function without arguments & with return value
 4. Funciton ka work kisi bhi specific problem ko solve karne ke liye kiya jata hai.
 5. Ek funciton ke andar aap multiple condtion, loops, function ya class tak call kar sakte hain.
+
+Function ka syntax:
+
+def your_function_name():
+    funciton body
+
+Funciton name ke limitations:
+
+1. Fucntion name ko bhi create karte time varibale ke rules ko dhyan rakho.
+2. Jaise new variable number se ya fir space se nahi start kar sakte ho. Same to same yaha par bhi wahi sare varaible ruls follow hote hain.
+
+Note: Yaad rahe complete programming lanaguage me hame 2 chije bar bar milti hain.
+
+1. Keywords: Jaise ke del, def, class etc...
+2. Function: Jaise ki print(), if(), for() etc....
 '''
+
+# Ye only fucntion defintion hai.
+# Kewal fucntion create karne se iska output nahi dekh sakte ho.
+def myFirstFucntion(): # ye fucntion definition hai
+    print('\nHello first function.')
+
+# Function ko use karne ke liye use call karna jarui hai.
+myFirstFucntion() # ye fucntion call hai
+
+
+
