ad7f2bbe14167ecd92f96b43da5c5c1ca3cf9c5b
heysushil <sushil.chaudhary111@gmail.com>
2020-09-09 14:14:47 +0530

    for loop updates

--- 16.for_loop.py
+++ 16.for_loop.py
@@ -45,6 +45,49 @@ for name in nameList:
 for number in range(1,51):
     print(number)
 
+# nested loop: means loop ke andar loop
+fields = ['name','mobile','address']
+names = ['ram','shyam']
+
+for f in fields: # 3 index
+    # print('Fiels Name: ', f)
+    for n in names: # 2 index
+        print(f, ' : ', n)
+    
+    print('\n')
+
+'''
+nested for loop case:
+
+f=name : names=ram
+f=name : names=shyam
+
+f=mobile : 
+
+for start hua: range(1, 5)
+    for start hua: (1, 5)
+
+'''
+a2d = ['a','b','c','d']
+
+for a in a2d:
+    for r in range(1,6):
+        print(a, ' : ', r)
+
+# fiels and thire data
+fields = ['name','mobile','address']
+names = ['ram',9989879879,'India']
+
+for f in fields:
+    for n in names:
+        print(f, ' : ', n)
+
+# use else in for loop
+for i in range(1, 6):
+    print('Number: ', i)
+else:
+    print('Finally our for loop of print number is finded.')
+
 '''
 Question:
 
@@ -75,4 +118,17 @@ Note: Question number 4 ke liye reserch ki jarurat padegi.
         
         b. But agar list me multiple dublicate values hain then is list ka use karke ye pata karna hai ki last 7 ka index number kya hai:
         [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7]
+
+5. 2 list hain jinke data ko side by side show karan hai:
+    Example lists:
+        fields = ['name','mobile','address']
+        names = ['ram',9989879879,'India']
+
+    For loop ka use karke inko side by side show karna hai?
+
+6. Jaise for loop me else ka use kiya gaya hai wise hi while loop me user se 2 numebrs ka input lena hai aur in case agar starting number 1 se greater hai to number not accetp ka message show karan hai.
+
+Otherwise agar end number greater then 10 hai to not accept ka mesage show karan hai.
+
+Otherwise numbers ko print karna hai and at last while loop end ke bad else ka use karke while loop stoped ka message show karna hai.
 '''
---
+++ 17.function.py
@@ -0,0 +1,32 @@
+'''
+About Function in Python:
+
+Function 2 type ke hote hain:
+
+    1. pre-defined function
+    2. user-defined function
+
+Pre-defined function: 
+
+1. Ye function programming lanaguage me pahle se define hote hain.
+2. In work aur behaviour pahle se set hota hai.
+3. Jaise ki example ke liye: print() function.
+    a. print function python me output show karne ke liye use hota hai.
+    b. Is funciton me number of aruments ka use kiya jata hai.
+    c. Jaise ki print me hum direct math solve kar sakte hain.
+    d. Koi dusra pre-define fucntion bhi use kar sakte hain. Jaise ki datatype check karne ke liye type() function.
+4. Har pre-define fucntion ka work behaviour bilkul user-define function jaisa hi hota hai. Bas in funciton ko pahle se hi define kar diya gaya hai.
+5. Jin funciton ya work behavious ko pahle se define nahi kiya gaya hai. Un ke liye hi hum user-define fucniton create karte hain.
+
+User-defined function:
+
+1. User-define funciton ka name user define karta hai.
+2. Funciton name hamesa relevent name hona cahiye. Taki name se function ka work behaviour pata chale.
+3. Function ke sub types bhi hain:
+    a. Function with arguments & with return value
+    b. Function without arguments & without return value
+    c. Function with arguments & without return value
+    d. Function without arguments & with return value
+4. Funciton ka work kisi bhi specific problem ko solve karne ke liye kiya jata hai.
+5. Ek funciton ke andar aap multiple condtion, loops, function ya class tak call kar sakte hain.
+'''
